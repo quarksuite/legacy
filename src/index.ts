466a1f124dcf1d918b68ff44780cf8855aafa19a
Chatman R. Jr <crjr.code@gmail.com>
2019-07-08 03:55:42 -0400

    Adds Colors and Quarks namespaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export const quarks = {
+import {Custom} from './colors/custom';
+import {Scheme} from './colors/scheme';
 
+import {Utility} from './utils';
+import {Config} from './config';
+
+/**
+ * Module encapsulating [[Custom]], [[Scheme]] color configuration modules
+ */
+export const Colors = {
+  Custom,
+  Scheme
+}
+
+/**
+ * Module encapsulating [[Utility]] and [[Config]] helper modules.
+ */
+export const Quarks = {
+  Config,
+  Utility
 }
