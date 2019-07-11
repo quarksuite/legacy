7591d56442e3ec1846641ba626d5820e778b152c
Chatman R. Jr <crjr.code@gmail.com>
2019-07-11 19:11:27 -0400

    Moves interfaces to utils directory.

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,49 @@
+import {InterpolationMode} from 'chroma-js';
+
+export interface QuarksilverConfigSchema {
+  colors: ColorCustomSwatchSchema | ColorCustomPaletteSchema | ColorBasicPaletteSchema,
+  content: ContentSchema
+}
+
+export interface ContentSchema {
+  fonts: ContentFontsSchema,
+  scale: ContentScaleSchema
+}
+
+export interface ColorCustomSwatchSchema {
+  [index: string]: string
+}
+
+export interface ColorCustomPaletteSchema {
+  [index: string]: {
+    value: string,
+    options?: ColorOptionAdjustments
+  }
+}
+
+export interface ColorBasicPaletteSchema {
+  base: string,
+  scheme?: 'monochromatic' | 'complementary' | 'split complement' | 'triadic' | 'clash' | 'analogous',
+  options?: ColorOptionAdjustments,
+  neutral?: boolean
+}
+
+export interface ColorOptionAdjustments {
+  range?: number | 'minimal' | 'material',
+  mode?: InterpolationMode,
+  contrast?: number | 'low' | 'med' | 'high'
+}
+
+export interface ContentFontsSchema {
+  [index: string]: {
+    name: string,
+    stack: string | string[],
+    styles: Array<string | number>
+  }
+}
+
+export interface ContentScaleSchema {
+  base: string,
+  ratio: number |  'augmented4th' | 'goldenRatio' | 'minor2nd' | 'minor3rd' | 'major2nd' | 'major3rd' | 'perfect4th' | 'perfect5th',
+  range: number | [number, number] | 'fullScale' | 'half' | 'minimal'
+}
