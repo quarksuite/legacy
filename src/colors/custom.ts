853fe79f397b4fdeba625df1982286e57b6e4e7b
Chatman R. Jr <crjr.code@gmail.com>
2019-07-16 11:55:14 -0400

    Document custom colors module

--- src/colors/custom.ts
+++ src/colors/custom.ts
@@ -2,12 +2,48 @@ import chroma from 'chroma-js';
 import { ColorCustomSwatchSchema, ColorCustomPaletteSchema } from '../schema';
 import { tokenize, shades, tints } from './kit';
 
+/**
+ * Outputs a collection of swatch tokens from a custom color palette.
+ *
+ * ```ts
+ * import {swatches} from '@quarksilver/core';
+ *
+ * const data = {
+ *   red: '#f00',
+ *   green: '#0f0',
+ *   blue: '#00f'
+ * }
+ *
+ * swatches(data);
+ * ```
+ */
 export const swatches = (data: ColorCustomSwatchSchema): object =>
   Object.keys(data).reduce((container, key: string): object => {
     const color = chroma(data[key]).hex();
     return { ...container, ...tokenize(color, key) };
   }, {});
 
+/**
+ * Outputs palette tokens from color category objects
+ *
+ * ```ts
+ * import {palette} from '@quarksilver/core';
+ *
+ * const data = {
+ *   red: {
+ *     base: '#f00'
+ *   }
+ *   green: {
+ *     base: '#0f0'
+ *   }
+ *   blue: {
+ *     base: '#00f'
+ *   }
+ * }
+ *
+ * palette(data);
+ * ```
+ */
 export const palette = (data: ColorCustomPaletteSchema) =>
   Object.keys(data).reduce((container, category: string) => {
     const { base, options } = data[category];
