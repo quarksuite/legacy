4252bb396bcccb484372f1154f5a4dd62e23e723
Chatman R. Jr <crjr.code@gmail.com>
2019-09-13 16:52:20 -0400

    Updates and tests content.scale.output

---
+++ tests/content/scale/output.spec.ts
@@ -0,0 +1,18 @@
+import { create, modify, output } from '../../../src/content/scale';
+
+describe('Scale creation and modification', () => {
+  describe('scale.output(scale, unit?, precision?)', () => {
+    test('outputs a system ready scale with units', () => {
+      const scale = create(1, 'golden');
+      const data = output(modify(scale, 2, (n, value) => n * value));
+      expect(data).toStrictEqual([
+        '2rem',
+        '3.236rem',
+        '5.236rem',
+        '8.472rem',
+        '13.71rem',
+        '22.18rem'
+      ]);
+    });
+  });
+});
