39d7377e296b0cf55260ae841d188bc992cf6b16
Chatman R. Jr <crjr.code@gmail.com>
2020-03-26 19:57:46 -0400

    Allow uncurried scheme functions as well.

--- src/color/scheme/index.ts
+++ src/color/scheme/index.ts
@@ -6,33 +6,33 @@ const assemble = (scheme: Function[], color: string): string[] =>
 
 const scheme = curry(2, assemble);
 
-const splitOrigin = (offset: number): string[] =>
+const splitOrigin = (offset: number, color: string): string[] =>
   scheme([
     adjust('hue', (h: number) => h - offset),
     adjust('hue', (h: number) => h),
     adjust('hue', (h: number) => h + offset)
-  ]);
+  ])(color);
 
-const splitComplement = (offset: number): string[] =>
+const splitComplement = (offset: number, color: string): string[] =>
   scheme([
     adjust('hue', (h: number) => h),
     adjust('hue', (h: number) => h + 180 - offset),
     adjust('hue', (h: number) => h + 180 + offset)
-  ]);
+  ])(color);
 
-const spreadOC = (offset: number): string[] =>
+const spreadOC = (offset: number, color: string): string[] =>
   scheme([
     adjust('hue', (h: number) => h),
     adjust('hue', (h: number) => h + 180),
     adjust('hue', (h: number) => h + offset),
     adjust('hue', (h: number) => h + 180 + offset)
-  ]);
+  ])(color);
 
 export const complementary = scheme([
   adjust('hue', (h: number) => h),
   adjust('hue', (h: number) => h + 180)
 ]);
 
-export const analogous = curry(1, splitOrigin);
-export const triad = curry(1, splitComplement);
-export const tetrad = curry(1, spreadOC);
+export const analogous = curry(2, splitOrigin);
+export const triad = curry(2, splitComplement);
+export const tetrad = curry(2, spreadOC);
