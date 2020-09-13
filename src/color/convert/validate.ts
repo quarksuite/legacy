24ab555300e1b2477150ceaaaa2453f934c5431e
Chatman R. Jr <crjr.code@gmail.com>
2020-09-13 00:26:48 -0400

    Refactor and test color validation utility.

--- src/color/convert/validate.ts
+++ src/color/convert/validate.ts
@@ -1,25 +1,27 @@
 import { w3c } from "@color/data/w3c-x11";
 
-export const ColorError = (format: string): Error => {
-  return new Error("Invalid format: cannot be converted");
+const InvalidColorError = (input: string): Error => {
+  throw Error(`'${input}' is not a valid CSS color`);
 };
 
-export const parseColor = (color: string): string => {
+export const validateColor = (color: string): string | Error => {
   interface Categories {
     [index: string]: RegExp | boolean;
   }
 
   const formats: Categories = {
-    hex: /^#([\da-f]{3}){1,2}$/i,
-    rgb: /^rgb\((((((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5]),\s?)){2}|((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5])\s)){2})((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5]))|((((([1-9]?\d(\.\d+)?)|100|(\.\d+))%,\s?){2}|((([1-9]?\d(\.\d+)?)|100|(\.\d+))%\s){2})(([1-9]?\d(\.\d+)?)|100|(\.\d+))%))\)$/i,
-    hsl: /^hsl\(((((([12]?[1-9]?\d)|[12]0\d|(3[0-5]\d))(\.\d+)?)|(\.\d+))(deg)?|(0|0?\.\d+)turn|(([0-6](\.\d+)?)|(\.\d+))rad)((,\s?(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2}|(\s(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2})\)$/i,
+    hex: /^#([\da-f]{3,4}){1,2}$/i,
+    rgb: /^rgb\((((((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5]),\s?)){2}|((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5])\s)){2})((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5]))|((((([1-9]?\d(\.\d+)?)|100|(\.\d+))%,\s?){2}|((([1-9]?\d(\.\d+)?)|100|(\.\d+))%\s){2})(([1-9]?\d(\.\d+)?)|100|(\.\d+))%))\)$/,
+    rgba: /^rgba\((((((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5]),\s?)){3})|(((([1-9]?\d(\.\d+)?)|100|(\.\d+))%,\s?){3}))|(((((1?[1-9]?\d)|10\d|(2[0-4]\d)|25[0-5])\s){3})|(((([1-9]?\d(\.\d+)?)|100|(\.\d+))%\s){3}))\/\s)((0?\.\d+)|[01]|(([1-9]?\d(\.\d+)?)|100|(\.\d+))%)\)$/,
+    hsl: /^hsl\(((((([12]?[1-9]?\d)|[12]0\d|(3[0-5]\d))(\.\d+)?)|(\.\d+))(deg)?|(0|0?\.\d+)turn|(([0-6](\.\d+)?)|(\.\d+))rad)((,\s?(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2}|(\s(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2})\)$/,
+    hsla: /^hsla\(((((([12]?[1-9]?\d)|[12]0\d|(3[0-5]\d))(\.\d+)?)|(\.\d+))(deg)?|(0|0?\.\d+)turn|(([0-6](\.\d+)?)|(\.\d+))rad)(((,\s?(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2},\s?)|((\s(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2}\s\/\s))((0?\.\d+)|[01]|(([1-9]?\d(\.\d+)?)|100|(\.\d+))%)\)$/,
     w3c: !!w3c[color]
   };
 
-  return Object.keys(formats)
-    .filter((category: string) => {
-      if (category === "w3c") return formats[category];
-      return (formats[category] as RegExp).test(color);
-    })
-    .join("");
+  const isValid = Object.keys(formats).filter((category: string) => {
+    if (category === "w3c") return formats[category];
+    return (formats[category] as RegExp).test(color);
+  })[0];
+
+  return isValid || InvalidColorError(color);
 };
---
+++ tests/color/convert/validate.spec.ts
@@ -0,0 +1,42 @@
+import { validateColor } from "@color/convert/validate";
+
+describe("validateColor :: string -> boolean | Error", () => {
+  test("can validate hex colors", () => {
+    expect(validateColor("#000000")).toBeTruthy();
+    expect(validateColor("#aaaaaa")).toBeTruthy();
+    expect(validateColor("#ffffff")).toBeTruthy();
+    expect(validateColor("#00f")).toBeTruthy();
+    expect(validateColor("#0fa31011")).toBeTruthy();
+    expect(validateColor("#af15")).toBeTruthy();
+    expect(() => validateColor("#fqq000")).toThrow();
+    expect(() => validateColor("f0aa00")).toThrow();
+  });
+  test("can validate rgb colors", () => {
+    expect(validateColor("rgb(0, 0, 0)")).toBeTruthy();
+    expect(validateColor("rgb(128, 128, 128)")).toBeTruthy();
+    expect(validateColor("rgb(255, 255, 255)")).toBeTruthy();
+    expect(validateColor("rgb(20, 110, 19)")).toBeTruthy();
+    expect(validateColor("rgba(10, 210, 119, 0.1)")).toBeTruthy();
+    expect(validateColor("rgb(20%, 70%, 19%)")).toBeTruthy();
+    expect(validateColor("rgba(2%, 7%, 79%, 0.5)")).toBeTruthy();
+    expect(() => validateColor("rgb(301, 200, 1)")).toThrow();
+  });
+  test("can validate hsl colors", () => {
+    expect(validateColor("hsl(0, 0%, 0%)")).toBeTruthy();
+    expect(validateColor("hsl(0, 0%, 50%)")).toBeTruthy();
+    expect(validateColor("hsl(0, 0%, 100%)")).toBeTruthy();
+    expect(validateColor("hsl(304, 11%, 40%)")).toBeTruthy();
+    expect(validateColor("hsla(100, 11%, 40%, 0.75)")).toBeTruthy();
+    expect(validateColor("hsla(3.5rad, 70%, 89%, 0.1)")).toBeTruthy();
+    expect(validateColor("hsla(0.25turn, 70%, 89%, 0.1)")).toBeTruthy();
+    expect(() => validateColor("hsl(30h, 20, 1)")).toThrow();
+  });
+  test("can validate named colors", () => {
+    expect(validateColor("black")).toBeTruthy();
+    expect(validateColor("gray")).toBeTruthy();
+    expect(validateColor("white")).toBeTruthy();
+    expect(validateColor("dodgerblue")).toBeTruthy();
+    expect(validateColor("coral")).toBeTruthy();
+    expect(() => validateColor("evergreen")).toThrow();
+  });
+});
