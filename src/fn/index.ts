970aac3df9d9fa9e61d52d64640a82d9b28cb474
Chatman R. Jr <crjr.code@gmail.com>
2020-09-21 14:23:57 -0400

    Rewrite and expose functional utilities as part of the advanced API.

---
+++ src/fn/index.ts
@@ -0,0 +1,80 @@
+export type Unary<T, R> = (x: T) => R;
+export type Binary<T, U, R> = (y: U, x: T) => R;
+export type Ternary<T, U, V, R> = (z: V, y: U, x: T) => R;
+export type Variadic<T extends any[], U extends any[], R> = (
+  ...args: [...T, ...U]
+) => R;
+
+/**
+ * Composes two **unary** functions of any return type.
+ *
+ * @param g - second function
+ * @param f - first function
+ * @param x - the value to be composed
+ * @return `h(x) = g`
+ *
+ * @remarks
+ * If function `arity > 1`, you'll have to curry it to `1`.
+ *
+ * This library never composes more than three functions, so a classical binary
+ * compose utility is just right.
+ */
+export const compose = <T, U, R>(f: Unary<T, U>, g: Unary<U, R>) => (x: T) =>
+  g(f(x));
+
+/**
+ * A higher order function that accepts a binary function, and separates
+ * its parameters into a sequence of calls.
+ *
+ * @remarks
+ * It can also be used to short circuit a variadic function into a fixed arity.
+ *
+ * In this case, arity `2`
+ *
+ * @param fn - the function to curry
+ * @param y - arg
+ * @param x - data
+ * @return the args of `fn` as a sequence of 2 calls
+ *
+ */
+export const curry2 = <T, U, R>(fn: Binary<T, U, R>) => (y: U) => (x: T): R =>
+  fn(y, x);
+
+/**
+ * A higher order function that accepts a ternary function, and separates
+ * its parameters into a sequence of calls.
+ *
+ * @remarks
+ * It can also be used to truncate a variadic function into a fixed arity.
+ *
+ * In this case, arity `3`
+ *
+ * @param fn - the function to curry
+ * @param z - arg
+ * @param y - arg
+ * @param x - data
+ * @return the args of `fn` as a sequence of 3 calls
+ *
+ */
+export const curry3 = <T, U, V, R>(fn: Ternary<T, U, V, R>) => (z: V) => (
+  y: U
+) => (x: T): R => fn(z, y, x);
+
+/**
+ * A higher order function that accepts a variadic function, and separates
+ * its parameters into a sequence of calls.
+ *
+ * @remarks
+ * If called on a function of fixed arity, it enables partial application
+ * of its parameters
+ *
+ * @param fn - the function to curry
+ * @param applied - initial arguments
+ * @param remaining - remaining arguments
+ * @return the args of `fn` as a sequence of two calls of its arguments
+ *
+ */
+export const curryN = <T extends any[], U extends any[], R>(
+  fn: Variadic<T, U, R>,
+  ...applied: T
+) => (...remaining: U): R => fn(...applied, ...remaining);
---
+++ tests/fn/index.spec.ts
@@ -0,0 +1,49 @@
+import { compose, curry2, curry3, curryN } from "@fn/index";
+
+describe("Functional helpers", () => {
+  test("compose :: [Fn] -> Arg", () => {
+    const fn1 = (n: string): string => `fn1(${n})`;
+    const fn2 = (val: string): string => `fn2(${val})`;
+    const composed = compose(fn1, fn2);
+    const result = composed("value");
+
+    expect(result).toBe("fn2(fn1(value))");
+  });
+  test("curry2 :: (Fn) -> y -> x", () => {
+    const sum = curry2((y: number, x: number): number => x + y);
+
+    const sumOf10 = sum(10);
+    const completeSum = sum(3)(4);
+
+    expect(sumOf10(3)).toBe(13);
+    expect(completeSum).toBe(7);
+  });
+  test("curry3 :: (Fn) -> z -> y -> x", () => {
+    const aggregate = curry3((...values: number[]): number =>
+      values.reduce((a: number, b: number): number => a + b)
+    );
+
+    const a = aggregate(100);
+    const b = a(28);
+    const c = b(42);
+
+    expect(a(42)(75)).toBe(217);
+    expect(b(97)).toBe(225);
+    expect(c).toBe(170);
+  });
+
+  test("curryN :: (Fn) -> [b] -> [a] -> x", () => {
+    const aggregate = (...values: number[]): number =>
+      values.reduce((a: number, b: number): number => a + b);
+
+    const $aggregate1 = curryN(aggregate, 1);
+    const $aggregate2 = curryN(aggregate, 1, 2);
+    const $aggregate3 = curryN(aggregate, 1, 2, 4);
+    const $aggregateAll = curryN(aggregate, 1, 2, 4, 8);
+
+    expect($aggregate1(2, 4, 8)).toBe(15);
+    expect($aggregate2(4, 8)).toBe(15);
+    expect($aggregate3(8)).toBe(15);
+    expect($aggregateAll()).toBe(15);
+  });
+});
