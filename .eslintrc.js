a4b413f8097bc6697527e6f6b61a239e1f1ef5f4
Chatman R. Jr <crjr.code@gmail.com>
2020-09-21 16:42:54 -0400

    Update project settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  plugins: ['@typescript-eslint', 'prettier'],
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  plugins: ["@typescript-eslint", "eslint-plugin-tsdoc", "prettier"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports,
+    sourceType: "module", // Allows for the use of imports,
     tsConfigRootDir: __dirname,
-    project: ['./tsconfig.eslint.json']
-  }
+    project: ["./tsconfig.eslint.json"],
+  },
+  rules: {
+    "tsdoc/syntax": "warn",
+  },
 };
--- jest.config.js
+++ jest.config.js
@@ -4,19 +4,20 @@
 module.exports = {
   roots: ["<rootDir>/tests"],
   transform: {
-    "^.+\\.ts$": "ts-jest"
+    "^.+\\.ts$": "ts-jest",
   },
   testEnvironment: "node",
   setupFiles: ["core-js"],
   collectCoverage: true,
   displayName: {
     name: "@quarksuite/core",
-    color: "cyan"
+    color: "cyan",
   },
   moduleNameMapper: {
-    "^@architecture(.*)$": "<rootDir>/src/architecture$1",
+    "^@api(.*)$": "<rootDir>/src/index.ts",
+    "^@fn(.*)$": "<rootDir>/src/fn$1",
     "^@color(.*)$": "<rootDir>/src/color$1",
     "^@typography(.*)$": "<rootDir>/src/typography$1",
-    "^@scale(.*)$": "<rootDir>/src/scale$1"
-  }
+    "^@scale(.*)$": "<rootDir>/src/scale$1",
+  },
 };
