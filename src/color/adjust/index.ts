555df772bbee1170d985c2c75644ab36aae8c670
Chatman R. Jr <crjr.code@gmail.com>
2020-09-21 23:44:31 -0400

    Update adjustment functions to output correct formats.

--- src/color/adjust/index.ts
+++ src/color/adjust/index.ts
@@ -1,17 +1,22 @@
 import { compose } from "../../fn";
 import { percentAsFraction, percentAsFloat, normalization } from "../math";
-import { matchValues, extractNumber } from "../formatting";
 import { validateColor } from "../validate";
+import { extractHSL } from "../convert/hsl";
 import { toHSL, preserveFormat } from "../convert";
 
 export const hue = (n: number, color: string): string | Error => {
   // Reject invalid color
   validateColor("Cannot adjust hue of an invalid color", color);
 
-  const [h, S, L, A] = compose(toHSL, matchValues)(color);
-  const H = extractNumber(h) + n;
+  const [h, s, l, a] = compose(toHSL, extractHSL)(color);
 
-  const target = A ? `hsla(${H}, ${S}, ${L}, ${A})` : `hsl(${H}, ${S}, ${L})`;
+  const H = h + n;
+  const [S, L] = [s, l].map((value) => `${percentAsFloat(value)}%`);
+  const A = a != null ? a : 1;
+
+  const target =
+    A === 1 ? `hsl(${H}, ${S}, ${L})` : `hsla(${H}, ${S}, ${L}, ${A})`;
+
   return preserveFormat(target, color);
 };
 
@@ -19,10 +24,16 @@ export const saturation = (n: number, color: string): string | Error => {
   // Reject invalid color
   validateColor("Cannot adjust saturation of an invalid color", color);
 
-  const [H, s, L, A] = compose(toHSL, matchValues)(color);
-  const S = `${normalization(0, 100, extractNumber(s) + n)}%`;
+  const [h, s, l, a] = compose(toHSL, extractHSL)(color);
 
-  const target = A ? `hsla(${H}, ${S}, ${L}, ${A})` : `hsl(${H}, ${S}, ${L})`;
+  const H = h;
+  const S = `${normalization(0, 100, percentAsFloat(s) + n)}%`;
+  const L = `${percentAsFloat(l)}%`;
+  const A = a != null ? a : 1;
+
+  const target =
+    A === 1 ? `hsl(${H}, ${S}, ${L})` : `hsla(${H}, ${S}, ${L}, ${A})`;
+
   return preserveFormat(target, color);
 };
 
@@ -30,10 +41,16 @@ export const lightness = (n: number, color: string): string | Error => {
   // Reject invalid color
   validateColor("Cannot adjust lightness of an invalid color", color);
 
-  const [H, S, l, A] = compose(toHSL, matchValues)(color);
-  const L = `${normalization(0, 100, extractNumber(l) + n)}%`;
+  const [h, s, l, a] = compose(toHSL, extractHSL)(color);
 
-  const target = A ? `hsla(${H}, ${S}, ${L}, ${A})` : `hsl(${H}, ${S}, ${L})`;
+  const H = h;
+  const S = `${percentAsFloat(s)}%`;
+  const L = `${normalization(0, 100, percentAsFloat(l) + n)}%`;
+  const A = a != null ? a : 1;
+
+  const target =
+    A === 1 ? `hsl(${H}, ${S}, ${L})` : `hsla(${H}, ${S}, ${L}, ${A})`;
+
   return preserveFormat(target, color);
 };
 
@@ -41,9 +58,12 @@ export const alpha = (n: number, color: string): string | Error => {
   // Reject invalid color
   validateColor("Cannot adjust transparency of an invalid color", color);
 
-  const [H, S, L, a] = compose(toHSL, matchValues)(color);
+  const [h, s, l, a] = compose(toHSL, extractHSL)(color);
+
+  const H = h;
+  const [S, L] = [s, l].map((value) => `${percentAsFloat(value)}%`);
   const A = percentAsFraction(
-    normalization(0, 100, a ? percentAsFloat(extractNumber(a)) + n : 100 + n)
+    normalization(0, 100, a != null ? percentAsFloat(a) + n : 100 + n)
   );
 
   const target =
