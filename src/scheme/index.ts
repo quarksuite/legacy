4568c9fe2eca9b079deda335d6636325cbb9612d
Chatman R. Jr <crjr.code@gmail.com>
2020-09-22 19:28:31 -0400

    Add BasicScheme and CustomScheme types.

--- src/scheme/index.ts
+++ src/scheme/index.ts
@@ -1,26 +1,33 @@
+import {
+  Angle,
+  CSSColor,
+  PaletteRange,
+  BasicScheme,
+  CustomScheme,
+} from "../color/data/types";
 import { hue } from "../color/adjust";
 
-export const complementary = (color: string): (string | Error)[] => [
+export const complementary = (color: CSSColor): BasicScheme => [
   hue(0, color),
   hue(180, color),
 ];
 
 export const analogous = (
-  offset: number,
-  color: string
+  offset: Angle,
+  color: CSSColor
 ): (string | Error)[] => [
   hue(0, color),
   hue(-offset, color),
   hue(offset, color),
 ];
 
-export const triad = (offset: number, color: string): (string | Error)[] => [
+export const triad = (offset: Angle, color: CSSColor): BasicScheme => [
   hue(0, color),
   hue(180 + -offset, color),
   hue(180 + offset, color),
 ];
 
-export const tetrad = (offset: number, color: string): (string | Error)[] => [
+export const tetrad = (offset: Angle, color: CSSColor): BasicScheme => [
   hue(0, color),
   hue(180, color),
   hue(offset, color),
@@ -28,10 +35,10 @@ export const tetrad = (offset: number, color: string): (string | Error)[] => [
 ];
 
 export const custom = (
-  range: number,
-  offset: number,
-  color: string
-): (string | Error)[] =>
+  range: PaletteRange,
+  offset: Angle,
+  color: CSSColor
+): CustomScheme =>
   Array(range)
     .fill(offset)
     .map((value: number, i: number): string | Error => hue(value * i, color));
